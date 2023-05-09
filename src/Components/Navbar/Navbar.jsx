3dd338c211ab0361f21cca112e1ce2161180085e
Dima Karvackij <dimakarvackij777@gmail.com>
2023-05-09 23:12:44 +0300

    3

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -8,10 +8,10 @@ const Navbar = () => {
   return (
     <nav className={s.nav}>
       <div className={s.item}>
-        <NavLink to="/profile">Profile</NavLink>
+        <NavLink to="/profile">Profile</NavLink> 
       </div>
       <div className={`${s.item} ${s.active}`}>
-        <NavLink to="/dialogs">Messages</NavLink> 
+        <NavLink to="/dialogs">Dialogs</NavLink> 
       </div> 
       <div className={s.item}>
         <a>News</a>
@@ -22,7 +22,7 @@ const Navbar = () => {
       <div className={s.item}>
         <a>Settings</a>
       </div>
-    </nav>
+    </nav> 
   ); 
 };
 
